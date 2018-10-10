d0094677a7ef58e620b85fbd55ddcc40323059c3
Ryan James <ryanjames1729@gmail.com>
2018-10-10 08:10:44 -0500

    Create hactoberfest.py

---
+++ hactoberfest.py
@@ -0,0 +1,10 @@
+#This file's purpose is to give teachers a chance to contribute to a Python file openly. Add your own print statement, loop, function, or anything else to this.
+
+def getMyName():
+  return "Ryan James"
+ 
+for in range(4):
+  print("Ryan James")
+ 
+name = getMyName()
+print(name)
